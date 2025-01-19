b0d6ac5233014b10255db587d266928e09a78d65
Alain Mota <alainmota9@gmail.com>
2025-01-19 03:53:57 +0000

    frase amanda domingo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Alain muito bem!</h1>
+    return <h1> Obrigado meu amor pelo nosso passeio de domingo! Eu amei verdadeiramente! Te amo!</h1>
 }
 
 export default Home;
