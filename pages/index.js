1545b0da5864c65b9f0189584d9c29c56b4944dc
Alain Mota <alainmota9@gmail.com>
2025-02-16 03:18:37 +0000

    pagina de login

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,44 @@
-function Home() {
-  return <h1> texte </h1>;
-}
+import React from "react";
 
-function texte() {
-  return <h1> novo texte </h1>;
+class LoginPage extends React.Component {
+  constructor(proaps) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container d-flex justify-content-center">
+        <div className="card mt-5 w-50">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="nickName">Usuário</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nickName"
+                  placeholder="Usuário"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Senha</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Senha"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Entrar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
