4ace8a54112edee6fd6a582532c0d6c2f9745480
AlainMota9 <alainmota9@gmail.com>
2025-09-09 02:53:55 +0000

    Correção via Conteineres

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,6 @@
-import React from "react";
-
-class LoginPage extends React.Component {
-  constructor(proaps) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="container d-flex justify-content-center">
-        <div className="card mt-5 w-50">
-          <div className="card-body">
-            <form>
-              <div className="form-group">
-                <label htmlFor="nickName">Usuário</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="nickName"
-                  placeholder="Usuário"
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Senha</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password"
-                  placeholder="Senha"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Entrar
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function Home(){
+    return <h1> Obrigado meu amor pelo nosso passeio de domingo! Eu amei verdadeiramente! Te amo!</h1>
+    return <h1> texte </h1>
 }
 
 export default Home;
